b20d07c9ec7b45d906a9fa1826055ddfa227c11e
krishnakaranam <karanam.s@husky.neu.edu>
2018-10-11 19:30:50 -0700

    updated db_String

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,9 +4,7 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
-db_string = "postgres://vywhwbzvzxprkq:" +
-"2f7083fed0106103e25ce5300750f5a8af50678ae11710731e61692e7deab729" +
-"@ec2-54-225-76-201.compute-1.amazonaws.com:5432/d58h3832oj43d6"
+db_string = "postgres://vywhwbzvzxprkq:2f7083fed0106103e25ce5300750f5a8af50678ae11710731e61692e7deab729@ec2-54-225-76-201.compute-1.amazonaws.com:5432/d58h3832oj43d6"
 
 db = create_engine(db_string)
 
