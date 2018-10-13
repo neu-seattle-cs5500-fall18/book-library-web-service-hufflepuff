372881748418a1c9af14874b7b747abb32020fc4
krishnakaranam <karanam.s@husky.neu.edu>
2018-10-12 17:30:45 -0700

    commented some API routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,11 +6,11 @@ def init_api_routes(app):
     if app:
         app.add_url_rule('/api/users',
                          'get_users', get_users, methods=['GET'])
-        app.add_url_rule('/api/books',
-                         'get_books', get_books, methods=['GET'])
+        # app.add_url_rule('/api/books',
+        #                  'get_books', get_books, methods=['GET'])
         app.add_url_rule('/api/books',
                          'add_books', add_books, methods=['POST'])
-        app.add_url_rule('/api/books',
-                         'update_books', update_books, methods=['PUT'])
-        app.add_url_rule('/api/books',
-                         'remove_books', remove_books, methods=['DELETE'])
+        # app.add_url_rule('/api/books',
+        #                  'update_books', update_books, methods=['PUT'])
+        # app.add_url_rule('/api/books',
+        #                  'remove_books', remove_books, methods=['DELETE'])
