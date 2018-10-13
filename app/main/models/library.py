12facfc65266302e935767e5c43bfceb98e3c027
divyaagarwal <agarwal.d@husky.neu.edu>
2018-10-12 17:05:10 -0700

    Adding serialize and de

--- app/main/models/library.py
+++ app/main/models/library.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, String, Integer, Date, ForeignKey
 from .Model import Model
 
+from pyld import jsonld
+
 
 class Library(Model):
     """
@@ -15,9 +17,41 @@ class Library(Model):
     status = Column(String(20))
     published_date = Column(Date)
 
-    def __init__(self, book_id, name, author, subject, published_date):
+    def __init__(self, book_id, name, author, subject, published_date, status):
         self.book_id = book_id
         self.name = name
         self.author = author
         self.subject = subject
         self.published_date = published_date
+        self.status = status
+
+    def __repr__(self):
+        return '<bookid {}>'.format(self.book_id)
+
+    #
+    # METHODS
+    #
+
+    def serialize(self):
+        compacted_json = jsonld.compact({
+            "http://schema.org/book_id": self.book_id,
+            "http://schema.org/name": self.name,
+            "http://schema.org/author": self.author,
+            "http://schema.org/subject": self.subject,
+            "http://schema.org/status": self.status
+            "http://schema.org/published_date":published_date
+        }, self.get_context())
+        del compacted_json['@context']
+        return compacted_json
+
+    def get_context(self):
+        return {
+            "@context": {
+                "book_id": "http://schema.org/book_id",
+                "name": "http://schema.org/name",
+                "author": "http://schema.org/author",
+                "subject": "http://schema.org/subject",
+                "status": "http://schema.org/status",
+                "published_date":"http://schema.org/published_date"
+            }
+        }
