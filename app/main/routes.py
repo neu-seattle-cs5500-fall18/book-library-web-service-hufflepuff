c36f7fbaa95a6ee0c6b0903b3a387ff64e5c3e09
krishnakaranam <karanam.s@husky.neu.edu>
2018-10-17 16:36:28 -0700

    Update app/main/routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -56,160 +56,161 @@ def init_api_routes(app):
 
         @book_api.route('/api/books/<int:book_id>')
         class GetBooks(Resource):
-                @api.response(200, 'Success')
-                @api.response(404, 'Not Found')
-                @api.doc(params={'book_id': 'The book_id of the ' +
-                                 'book to be retrieved'})
+                @book_api.response(200, 'Success')
+                @book_api.response(404, 'Not Found')
+                @book_api.doc('list_books',
+                              params={'book_id': 'The book_id of the ' +
+                                      'book to be retrieved'})
                 def get(self, book_id):
                         return [book_id], 200
 
-                @api.response(200, 'Success')
-                @api.response(400, 'Validation Error')
-                @api.doc(params={'book_id': 'The book_id of the ' +
-                                 'book to be updated'})
-                @api.expect(add_book)
+                @book_api.response(200, 'Success')
+                @book_api.response(400, 'Validation Error')
+                @book_api.doc(params={'book_id': 'The book_id of the ' +
+                                      'book to be updated'})
+                @book_api.expect(add_book)
                 def put(self, book_id):
                         return {"message": "Book Updated Successfully"}, 200
 
-                @api.response(204, 'No Content')
-                @api.response(404, 'Not Found')
-                @api.doc(params={'book_id': 'The book_id of the ' +
-                                 'book to be deleted'})
+                @book_api.response(204, 'No Content')
+                @book_api.response(404, 'Not Found')
+                @book_api.doc(params={'book_id': 'The book_id of the ' +
+                                      'book to be deleted'})
                 def delete(self, book_id):
                         return {"message": "Book Deleted Successfully"}, 204
 
         @book_api.route('/api/books')
         class AddBooks(Resource):
-                @api.response(201, 'Created')
-                @api.response(400, 'Validation Error')
-                @api.expect(add_book)
+                @book_api.response(201, 'Created')
+                @book_api.response(400, 'Validation Error')
+                @book_api.expect(add_book)
                 def post(self):
                         return {"message": "Book Added Successfully"}, 201
 
         @user_api.route('/api/users/<int:user_id>')
         class GetUsers(Resource):
-                @api.response(200, 'Success')
-                @api.response(404, 'Not Found')
-                @api.doc(params={'user_id': 'The user_id of the ' +
-                                 'user to be retrieved'})
+                @user_api.response(200, 'Success')
+                @user_api.response(404, 'Not Found')
+                @user_api.doc(params={'user_id': 'The user_id of the ' +
+                                      'user to be retrieved'})
                 def get(self, user_id):
                         return [user_id], 200
 
-                @api.response(200, 'Success')
-                @api.response(400, 'Validation Error')
-                @api.doc(params={'user_id': 'The user_id of the ' +
-                                 'user to be updated'})
-                @api.expect(add_user)
+                @user_api.response(200, 'Success')
+                @user_api.response(400, 'Validation Error')
+                @user_api.doc(params={'user_id': 'The user_id of the ' +
+                                      'user to be updated'})
+                @user_api.expect(add_user)
                 def put(self, user_id):
                         return {"message": "User Updated Successfully"}, 200
 
-                @api.response(204, 'No Content')
-                @api.response(404, 'Not Found')
-                @api.doc(params={'user_id': 'The user_id of the ' +
-                                 'user to be deleted'})
+                @user_api.response(204, 'No Content')
+                @user_api.response(404, 'Not Found')
+                @user_api.doc(params={'user_id': 'The user_id of the ' +
+                                      'user to be deleted'})
                 def delete(self, user_id):
                         return {"message": "User Deleted Successfully"}, 204
 
         @user_api.route('/api/users')
         class AddUsers(Resource):
-                @api.response(201, 'Created')
-                @api.response(400, 'Validation Error')
-                @api.expect(add_user)
+                @user_api.response(201, 'Created')
+                @user_api.response(400, 'Validation Error')
+                @user_api.expect(add_user)
                 def post(self):
                         return {"message": "User Created Successfully"}, 201
 
         @note_api.route('/api/notes/<int:note_id>')
         class GetNotes(Resource):
-                @api.response(200, 'Success')
-                @api.response(404, 'Not Found')
-                @api.doc(params={'note_id': 'The note_id of the ' +
-                                 'note'})
+                @note_api.response(200, 'Success')
+                @note_api.response(404, 'Not Found')
+                @note_api.doc(params={'note_id': 'The note_id of the ' +
+                                      'note'})
                 def get(self, note_id):
                         return {"note": str(note_id)}, 200
 
-                @api.response(200, 'Success')
-                @api.response(400, 'Validation Error')
-                @api.doc(params={'note_id': 'The note_id of the ' +
-                                 'note to be updated'})
-                @api.expect(add_user)
+                @note_api.response(200, 'Success')
+                @note_api.response(400, 'Validation Error')
+                @note_api.doc(params={'note_id': 'The note_id of the ' +
+                                      'note to be updated'})
+                @note_api.expect(add_user)
                 def put(self, note_id):
                         return {"message": "Notes Updated Successfully"}, 200
 
-                @api.response(204, 'No Content')
-                @api.response(404, 'Not Found')
-                @api.doc(params={'note_id': 'The note_id of the ' +
-                                 'note to be deleted'})
+                @note_api.response(204, 'No Content')
+                @note_api.response(404, 'Not Found')
+                @note_api.doc(params={'note_id': 'The note_id of the ' +
+                                      'note to be deleted'})
                 def delete(self, note_id):
                         return {"message": "Notes Deleted Successfully"}, 204
 
         @note_api.route('/api/notes')
         class AddNotes(Resource):
-                @api.response(201, 'Created')
-                @api.response(400, 'Validation Error')
-                @api.expect(add_note)
+                @note_api.response(201, 'Created')
+                @note_api.response(400, 'Validation Error')
+                @note_api.expect(add_note)
                 def post(self):
                         return {"message": "Notes Created Successfully"}, 201
 
         @loan_api.route('/api/users/<int:user_id>/loans')
         class AddLoans(Resource):
-                @api.response(200, 'Success')
-                @api.response(404, 'Not Found')
-                @api.doc(params={'user_id': 'The user_id of the ' +
-                                 'loans to be retrieved for'})
+                @loan_api.response(200, 'Success')
+                @loan_api.response(404, 'Not Found')
+                @loan_api.doc(params={'user_id': 'The user_id of the ' +
+                                      'loans to be retrieved for'})
                 def get(self, user_id):
                         return [user_id], 200
 
-                @api.response(201, 'Created')
-                @api.response(400, 'Validation Error')
-                @api.expect(add_loan)
+                @loan_api.response(201, 'Created')
+                @loan_api.response(400, 'Validation Error')
+                @loan_api.expect(add_loan)
                 def post(self):
                         return {"message": "User Created Successfully"}, 201
 
         @loan_api.route('/api/loans/<int:loan_id>')
         class GetLoans(Resource):
-                @api.response(200, 'Success')
-                @api.response(400, 'Validation Error')
-                @api.doc(params={'loan_id': 'The loan_id of the ' +
-                                 'loan to be updated'})
-                @api.expect(add_loan)
+                @loan_api.response(200, 'Success')
+                @loan_api.response(400, 'Validation Error')
+                @loan_api.doc(params={'loan_id': 'The loan_id of the ' +
+                                      'loan to be updated'})
+                @loan_api.expect(add_loan)
                 def put(self, loan_id):
                         return {"message": "Loan Updated Successfully"}, 200
 
-                @api.response(204, 'No Content')
-                @api.response(404, 'Not Found')
-                @api.doc(params={'loan_id': 'The loan_id of the ' +
-                                 'loan to be deleted'})
+                @loan_api.response(204, 'No Content')
+                @loan_api.response(404, 'Not Found')
+                @loan_api.doc(params={'loan_id': 'The loan_id of the ' +
+                                      'loan to be deleted'})
                 def delete(self, loan_id):
                         return {"message": "Loan Deleted Successfully"}, 204
 
         @list_api.route('/api/users/<int:user_id>/lists')
         class AddLists(Resource):
-                @api.response(200, 'Success')
-                @api.response(404, 'Not Found')
-                @api.doc(params={'user_id': 'The user_id of the ' +
-                                 'lists to be retrieved for'})
+                @list_api.response(200, 'Success')
+                @list_api.response(404, 'Not Found')
+                @list_api.doc(params={'user_id': 'The user_id of the ' +
+                                      'lists to be retrieved for'})
                 def get(self, user_id):
                         return [user_id], 200
 
-                @api.response(201, 'Created')
-                @api.response(400, 'Validation Error')
-                @api.expect(add_list)
+                @list_api.response(201, 'Created')
+                @list_api.response(400, 'Validation Error')
+                @list_api.expect(add_list)
                 def post(self):
                         return {"message": "List Created Successfully"}, 201
 
         @list_api.route('/api/lists/<int:list_id>')
         class GetLists(Resource):
-                @api.response(200, 'Success')
-                @api.response(400, 'Validation Error')
-                @api.doc(params={'list_id': 'The list_id of the ' +
-                                 'list to be updated'})
-                @api.expect(add_loan)
+                @list_api.response(200, 'Success')
+                @list_api.response(400, 'Validation Error')
+                @list_api.doc(params={'list_id': 'The list_id of the ' +
+                                      'list to be updated'})
+                @list_api.expect(add_loan)
                 def put(self, list_id):
                         return {"message": "List Updated Successfully"}, 200
 
-                @api.response(204, 'No Content')
-                @api.response(404, 'Not Found')
-                @api.doc(params={'list_id': 'The list_id of the ' +
-                                 'list to be deleted'})
+                @list_api.response(204, 'No Content')
+                @list_api.response(404, 'Not Found')
+                @list_api.doc(params={'list_id': 'The list_id of the ' +
+                                      'list to be deleted'})
                 def delete(self, list_id):
                         return {"message": "List Deleted Successfully"}, 204
