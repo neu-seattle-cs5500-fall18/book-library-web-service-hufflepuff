6c263ac57144cd32d4fc65553c12bb0b4e2d6205
krishna karanam <karanam.s@husky.neu.edu>
2018-12-11 19:00:26 -0800

    Update data_provider_service.py

--- app/main/data_provider_service.py
+++ app/main/data_provider_service.py
@@ -116,8 +116,8 @@ class DataProviderService:
         """
         the_loan = self.session.query(Loan).filter_by(user_id=user.user_id,
                                                       book_id=book.book_id,
-                                                      status="loaned out").first()
-        #the_loan.return_by = return_by
+                                                      status="Borrowed").first()
+        the_loan.return_by = return_by
         self.session.add(the_loan)
         self.session.commit()
         line1 = f'Hello, {user.name}!\nThe library wants you to return the book '
